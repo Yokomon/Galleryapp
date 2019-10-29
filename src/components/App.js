50cb6585d33c54b8b02551831273e2cb7e9cbe1d
Yokomon <marowmars@gmail.com>
2019-10-29 23:51:54 +0100

    I added stylesheet for the SearchBar

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import SearchBar from "./SearchBar"
+import "./SearchBar.css";
 
 const App = () => {
-    return (<div className="ui container">
+    return (<div className="ui container" id="formDetails">
         <SearchBar />
         </div>)
 }
