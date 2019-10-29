9be70b051fc4c9bbe257ac00088134f44640bf86
Yokomon <marowmars@gmail.com>
2019-10-29 22:17:15 +0100

    I have added the component folder

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
+import App from "./components/App";
 
 class App extends React.Component {
     render(){
