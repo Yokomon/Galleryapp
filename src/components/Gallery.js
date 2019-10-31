5d23281096856dc2e081c2fe68a2d46786bc66af
Yokomon <marowmars@gmail.com>
2019-10-31 19:28:11 +0100

    added a grid styling

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -5,7 +5,6 @@ const Gallery = props => {
  const images = props.images.map(({description,id,urls}) =>{
  return <img alt ={description} key={id} src={urls.regular }/>
 })
-    
     return <div className="image-list">{images}</div>
 }
 
