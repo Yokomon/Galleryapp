7605c22ae2b1628033e2234d783b45feed877ef5
Yokomon <marowmars@gmail.com>
2019-10-31 18:50:36 +0100

    added a stylesheet for the gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,11 +1,12 @@
+import "./Gallery.css";
 import React from 'react';
 
 const Gallery = props => {
  const images = props.images.map(({description,id,urls}) =>{
- return <img alt ={description} keys={id} src={urls.regular }/>
+ return <img alt ={description} key={id} src={urls.regular }/>
 })
     
-    return <div>{images}</div>
+    return <div className="image-list">{images}</div>
 }
 
 export default Gallery;
