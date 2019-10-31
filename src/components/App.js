951a0eca5e79ad7e19072943499c9ea790bbf8b7
Yokomon <marowmars@gmail.com>
2019-10-31 10:15:42 +0100

    created an Async function and other features

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from "./SearchBar"
 
 
 class App extends React.Component{
     onSearchSubmit(searchTerm){
-        console.log(searchTerm);
+        axios.get("https://api.unsplash.com/search/photos",{
+        params:{ query: searchTerm},
+        headers: {
+            Authorization: "Client-ID 31ce87b9ab117506482470ea3fa3321ad3ae078b4c0a685d16053664d996cd40"
+        }
+        }).then((response)=>{
+            console.log(response)
+        })
     }
 
     render(){
