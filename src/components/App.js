d577ebc0a66f6b310bb88c1cd08beacdfb12b665
Yokomon <marowmars@gmail.com>
2019-10-31 10:22:24 +0100

    added the async await method

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,15 @@ import SearchBar from "./SearchBar"
 
 
 class App extends React.Component{
-    onSearchSubmit(searchTerm){
-        axios.get("https://api.unsplash.com/search/photos",{
+    async onSearchSubmit(searchTerm){
+        const response = await axios.get("https://api.unsplash.com/search/photos",{
         params:{ query: searchTerm},
         headers: {
             Authorization: "Client-ID 31ce87b9ab117506482470ea3fa3321ad3ae078b4c0a685d16053664d996cd40"
         }
-        }).then((response)=>{
-            console.log(response)
-        })
+        });
+
+        console.log(response.data.results)
     }
 
     render(){
