a9b1bf3a37bf7fc58363de0627cca9778014a484
Yokomon <marowmars@gmail.com>
2019-10-31 11:13:57 +0100

    added an Api folder with axios.create method

---
+++ src/Api/unsplash.js
@@ -0,0 +1,8 @@
+import Axios from 'axios';
+
+export default Axios.create({
+    baseURL: "https://api.unsplash.com",
+    headers: {
+        Authorization: "Client-ID 31ce87b9ab117506482470ea3fa3321ad3ae078b4c0a685d16053664d996cd40"
+    }
+})
--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import axios from 'axios';
+import unsplash from '../Api/unsplash'
 import SearchBar from "./SearchBar"
 
 
@@ -7,11 +7,8 @@ class App extends React.Component{
     state = { images: [] }
     
     onSearchSubmit = async (searchTerm) => {
-        const response = await axios.get("https://api.unsplash.com/search/photos",{
+        const response = await unsplash.get("/search/photos",{
         params:{ query: searchTerm},
-        headers: {
-            Authorization: "Client-ID 31ce87b9ab117506482470ea3fa3321ad3ae078b4c0a685d16053664d996cd40"
-        }
         });
 
       this.setState({images: response.data.results})
