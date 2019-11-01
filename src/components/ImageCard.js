ba77b59157cf67cbe6850fd8a216af93df05a336
Yokomon <marowmars@gmail.com>
2019-11-01 22:06:43 +0100

    Added a Ref system

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
 class ImageCard extends React.Component{
+    constructor(props){
+        super(props)
+
+        this.imageRef = React.createRef
+    }
     render(){
         const {description, urls}= this.props.image;
         return(
